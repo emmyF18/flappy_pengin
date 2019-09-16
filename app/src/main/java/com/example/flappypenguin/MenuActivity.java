93674428b85ee4a7dcd252f4abe9f76fa58f0cc7
Brooke Schoen <brooke.schoen@my.simpson.edu>
2019-09-16 10:59:06 -0500

    Menu settings

--- app/src/main/java/com/example/flappypenguin/MenuActivity.java
+++ app/src/main/java/com/example/flappypenguin/MenuActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 public class MenuActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
-
     }
 
-
+    private void goToGameScreen()
+    {
+        final Intent game = new Intent(this, MainActivity.class);
+    }
 }
