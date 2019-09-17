8dba43eb8cf5774a482eb88a2242fa58e848d30a
Emmy <emmyfarrell18@gmail.com>
2019-09-17 18:04:13 -0500

    Penguin move code start
    it still doesn't move down though...

--- app/src/main/java/com/example/flappypenguin/MainActivity.java
+++ app/src/main/java/com/example/flappypenguin/MainActivity.java
@@ -2,18 +2,23 @@ package com.example.flappypenguin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.content.Intent;
 import android.os.CountDownTimer;
 import android.view.View;
+import android.widget.Toast;
+import android.widget.ImageButton;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.ViewSwitcher;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends AppCompatActivity
 {
     private Integer[] countdownImages = {R.drawable.countdown_3, R.drawable.countdown_2, R.drawable.countdown_1, R.drawable.countdown_start};
     private ImageSwitcher imageSwitcher;
+    private ImageButton imageButton;
     private int position = 0;
 
     @Override
@@ -22,6 +27,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        listenForButton();
         imageSwitcher = findViewById(R.id.countdown);
         imageSwitcher.setFactory(new ViewSwitcher.ViewFactory()
         {
@@ -40,6 +46,8 @@ public class MainActivity extends AppCompatActivity
         // SOURCE: https://www.tutlane.com/tutorial/android/android-imageswitcher-with-examples
 
         startCountdown();
+        moveDown();
+
     }
 
     private void startCountdown()
@@ -67,4 +75,27 @@ public class MainActivity extends AppCompatActivity
         }.start();
     }
     // SOURCE: https://abhiandroid.com/ui/countdown-timer
+    private void listenForButton()
+    {
+        imageButton = findViewById(R.id.penguin);
+        imageButton.setOnClickListener(new OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                imageButton.setY(imageButton.getY()-50);
+            }
+
+        });
+    }
+    //click code from https://www.mkyong.com/android/android-imagebutton-example/
+    private void moveDown()
+    {
+        imageButton = findViewById(R.id.penguin);
+        while(imageButton.getY() > findViewById(R.id.gameBackground).getBottom())
+        {
+            imageButton.setY(imageButton.getY() + 50);
+        }
+    }
+
 }
