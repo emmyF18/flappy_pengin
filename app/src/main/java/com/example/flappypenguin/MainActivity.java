81dfa1757d967b088f2473c37d0cc1fee31c97c0
Brooke Schoen <brooke.schoen@my.simpson.edu>
2019-09-25 12:50:00 -0500

    Got obstacles to appear in random

--- app/src/main/java/com/example/flappypenguin/MainActivity.java
+++ app/src/main/java/com/example/flappypenguin/MainActivity.java
@@ -3,12 +3,10 @@ package com.example.flappypenguin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.content.Intent;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.view.View;
-import android.widget.Toast;
 import android.widget.ImageButton;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
@@ -18,12 +16,18 @@ import android.view.View.OnClickListener;
 import com.daasuu.ei.Ease;
 import com.daasuu.ei.EasingInterpolator;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity
 {
-    private Integer[] countdownImagesList = {R.drawable.countdown_3, R.drawable.countdown_2, R.drawable.countdown_1, R.drawable.countdown_start, R.drawable.blank};
+    final private Integer[] countdownImagesList = {R.drawable.countdown_3, R.drawable.countdown_2, R.drawable.countdown_1, R.drawable.countdown_start, R.drawable.blank};
+    final private Integer[] obstacleImagesList = {R.drawable.ice_obstacle, R.drawable.ice_obstacle2, R.drawable.snowman_obstacle};
     private ImageSwitcher countdownImageSwitcher;
     private ImageButton penguinImage;
+    private ImageView imageView;
     private int countdownImagesPosition = 0;
+    private int randomTimer;
+    private int randomObstacle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,13 +37,14 @@ public class MainActivity extends AppCompatActivity
 
         // SOURCE: https://www.tutlane.com/tutorial/android/android-imageswitcher-with-examples
         listenForButton();
+
         countdownImageSwitcher = findViewById(R.id.countdown);
         countdownImageSwitcher.setFactory(new ViewSwitcher.ViewFactory()
         {
             @Override
             public View makeView()
             {
-                ImageView imageView = new ImageView(MainActivity.this);
+                imageView = new ImageView(MainActivity.this);
                 imageView.setImageResource(countdownImagesList[countdownImagesPosition]);
 
                 return imageView;
@@ -49,6 +54,9 @@ public class MainActivity extends AppCompatActivity
         countdownImageSwitcher.setInAnimation(this, android.R.anim.fade_in);
         countdownImageSwitcher.setOutAnimation(this, android.R.anim.fade_out);
         startCountdown();
+
+        imageView = findViewById(R.id.obstacles);
+        displayObstaclesRandomly();
 
         moveDown();
     }
@@ -112,5 +120,27 @@ public class MainActivity extends AppCompatActivity
         movePenguinUp.setStartDelay(500);
         movePenguinUp.setDuration(1500);
         movePenguinUp.start();
+    }
+
+    // SOURCE: https://stackoverflow.com/questions/21559405/how-to-display-image-automatically-after-a-random-time
+    private void displayObstaclesRandomly()
+    {
+        final Random random = new Random();
+        randomTimer = random.nextInt(4000 - 3000) + 3000;
+        randomObstacle = random.nextInt(obstacleImagesList.length);
+
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                imageView.setImageResource(obstacleImagesList[randomObstacle]);
+                handler.postDelayed(this, randomTimer);
+                randomObstacle = random.nextInt(obstacleImagesList.length);
+            }
+        };
+
+        handler.postDelayed(runnable, randomTimer);
     }
 }
